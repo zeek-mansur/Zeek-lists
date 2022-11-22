bbe0e4e6d217d4cd41cb4f57ec8cc79d3799a810
Zeek <mzulkiful@yahoo.com>
2022-11-22 21:21:03 +0000

    Adding Layout Component

---
+++ pages/components/Layout.js
@@ -0,0 +1,16 @@
+import Footer from "./Footer";
+import Navbar from "./NavBar";
+
+const Layout = ({children}) => {
+    return ( 
+
+        <div className="content">
+
+            <Navbar />
+            {children}
+            <Footer />
+        </div>
+     );
+}
+ 
+export default Layout;
