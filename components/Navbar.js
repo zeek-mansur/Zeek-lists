2c23e249e731cdb7a974813c24059ec59f71f4ea
Zeek <mzulkiful@yahoo.com>
2022-11-24 13:22:25 +0000

    Added Single item fetch

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,3 +1,4 @@
+/* eslint no-use-before-define: 0 */
 import Link from "next/link"
 import Image from 'next/image'
 
--- pages/zeek/[id].js
+++ pages/zeek/[id].js
@@ -1,8 +1,40 @@
-const Details = () => {
-    return ( 
 
+
+export const getStaticPaths = async () => {
+
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await res.json();
+
+    const paths = data.map(zeek => {
+        return{  
+        params: { id: zeek.id.toString() }
+            }
+    })
+
+    return {
+        paths,
+        fallback: false
+    }
+}
+
+export const getStaticProps = async (context) => {
+        const id = context.params.id
+        const res = await fetch('https://jsonplaceholder.typicode.com/users/' + id);
+        const data = await res.json();
+
+        return {
+            props: {zeek: data}
+        }
+}
+
+const Details = ({zeek}) => {
+    return ( 
         <div>
-            <h1>Details Page</h1>
+           <h1>{zeek.name}</h1>
+           <p>{zeek.email}</p>
+           <p>{zeek.website}</p>
+           <p>{zeek.address.city}</p>
+           
         </div>
      );
 }
