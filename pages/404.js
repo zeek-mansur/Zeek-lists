7ef197c77cf993031fb67f80b8be56d8047f6e39
Zeek <mzulkiful@yahoo.com>
2022-11-24 07:47:10 +0000

    404 page added and styled

---
+++ pages/404.js
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+const NotFound = () => {
+    return ( 
+        <div className="not-found">
+            <h1>Ooooops.....</h1>
+            <h2> That page cannot be found.</h2>
+            <p>Go back to the <Link href='/'>Homepage</Link></p>
+
+        </div>
+     );
+}
+ 
+export default NotFound;
